684217ebbc0c88f9dce761b448f6416e4c38c41f
mo <mo@moohh.xyz>
2020-10-14 14:47:46 -0400

    getting things working.

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,13 +7,35 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import firebase from 'firebase/app';
+import 'firebase/functions';
+import 'firebase/analytics';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
   },
-  async created() {},
+  async created() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyClrNOXnAsLnRFZxj-WJRG9FbQoBhoAq3I',
+      authDomain: 'iammoo-dice-roller.firebaseapp.com',
+      databaseURL: 'https://iammoo-dice-roller.firebaseio.com',
+      projectId: 'iammoo-dice-roller',
+      storageBucket: 'iammoo-dice-roller.appspot.com',
+      messagingSenderId: '1094719742577',
+      appId: '1:1094719742577:web:2d348a30e369b37d8331d0',
+      measurementId: 'G-PWQHV5S6FB',
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    let functions = firebase.functions();
+    let ping = functions.httpsCallable('ping');
+    console.log('ifjewaoifejw');
+    ping().then((res) => {
+      console.log(res.data);
+    });
+  },
 };
 </script>
 
