2d3d764076636c98400f894858a760887d5b3b00
mo <mo@moohh.xyz>
2020-10-15 13:03:28 -0400

    added passport-local auth

---
+++ server/src/auth/index.js
@@ -0,0 +1,119 @@
+const { Router } = require('express');
+const mongoose = require('mongoose');
+const LocalStrategy = require('passport-local').Strategy;
+const Users = require('./users');
+const logger = require('../logger').logger('auth');
+
+const configs = require('../configs');
+
+mongoose.connect(configs.MONGODB_URI);
+
+const passportSetup = (passport) => {
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    passport.deserializeUser((id, done) => {
+        User.fineOne({ _id: id }, '-password -salt', (e, user) => {
+            done(e, user);
+        });
+    });
+
+    // Local Register
+    passport.use('local-register', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    (req, email, password, done) => {
+        process.nextTick(() => {
+            Users.findOne({ 'email': email }, (e, user) => {
+                if (e) {
+                    logger.error(e);
+                    return done(e);
+                }
+
+                if (user) {
+                    return done(null, false, `User ${email} already exists`);
+                } else {
+                    var newUser = new Users();
+                    newUser.username = email;
+                    newUser.email = email;
+                    const salt = newUser.generateSalt();
+                    newUser.password = newUser.generateHash(password, salt);
+                    newUser.salt = salt;
+
+                    newUser.save((e) => {
+                        if (e) {
+                            throw e;
+                        }
+
+                        logger.info(`New user ${newUser.username} created`);
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+    }));
+
+    // Local Login
+    passport.use('local-login', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    (req, email, password, done) => {
+        process.nextTick(() => {
+            Users.findOne({ 'email': email }, (e, user) => {
+                if (e) {
+                    logger.error(e);
+                    return done(e);
+                }
+    
+                if (!user) {
+                    return done(null, false, 'No user found');
+                }
+    
+                if (!user.validPassword(password)) {
+                    logger.info(`Failed login for user ${user.username}`);
+                    return done(null, false, 'Password invalid');
+                }
+    
+                logger.info(`User ${user.email} logged in`);
+                return done(null, user);
+            })
+        });   
+    }));
+};
+
+// Routegaurd to verify that request is authenticated
+const isAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+};
+
+// Security router to easily attach /register and /login routes
+const securityRouter = (passport) => {
+    const router = Router();
+
+    // Register route
+    router.route('/register').post(passport.authenticate('local-register', {
+        successRedirect: '/',
+        failureRedirect: '/register?message=register-failed',
+    }));
+
+    // Login route
+    router.route('/login').post(passport.authenticate('local-login', {
+        successRedirect: '/',
+        failureRedirect: '/login?message=login-failed',
+    }));
+
+    return router;
+};
+
+module.exports = {
+    setup: passportSetup,
+    isAuth,
+    router: securityRouter,
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,12 @@
 const e = require('express');
+const session = require('express-session');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const passport = require('passport');
 const _ = require('lodash');
 const logger = require('./logger').logger('app');
+const auth = require('./auth');
 
 const configs = require('./configs');
 
@@ -11,14 +14,27 @@ let app = e();
 app.use(cors());
 app.use(helmet());
 app.use(morgan(configs.MORGAN_FORMAT));
+app.use(session({
+    secret: 'hotswap-on-the-downstroke',
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(e.json());
 
 app.get('/ping', (req, res) => {
     res.json({ message: 'pong' });
 });
 
+// Setup auth
+auth.setup(passport);
+app.use('/auth', auth.router(passport));
+
+// Setup app routes
+// Routing controllers can be found at /routes/index.js
 const routes = require('./routes');
-app.use('/api', routes)
+app.use('/api', auth.isAuth, routes)
 
 // Handle static front-end client for PROD
 if (process.env.NODE_ENV === 'production') {
@@ -26,6 +42,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 };
 
+// Error handler
 app.use((e, req, res, next) => {
     if (e.status) {
         res.status(e.status);
