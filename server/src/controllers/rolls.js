486aea9878aac4492faf947640b7293e9c654e05
Mohammed Hussain <moohh91@gmail.com>
2020-10-21 19:47:27 -0400

    working on rolls

--- server/src/controllers/rolls.js
+++ server/src/controllers/rolls.js
@@ -1,3 +1,39 @@
-module.exports = {
+const rollDb = require('../db/rolls');
 
+const getRollsForUser = async (req, res) => {
+    const { user } = req;
+
+    res.json('notimpl');
+};
+
+const roll = async (req, res) => {
+    const { sessionId, characterId, dvalue } = req.body;
+
+    const rng = Math.ceil(Math.random() * dvalue);
+    const roll = {
+        sessionId,
+        characterId,
+        dvalue,
+        rng,
+        'roll': rng,
+        rollTime: newDate(),
+        visible: false,
+    };
+
+    rollDb
+        .validate(roll)
+        .then(async (val) => {
+            const createdRoll = await rollDb.create(roll);
+            res.json(createdRoll);
+        })
+        .catch(({ name, erors }) => {
+            res.status(500).json({ name, errors });
+        });
+};
+
+module.exports = {
+    '/roll': {
+        get: getRollsForUser,
+        post: roll
+    }
 };
