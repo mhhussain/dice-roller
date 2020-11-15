6e1697bc05bfd6b4a1256a9396f2ff76da64cb96
Mohammed Hussain <moohh91@gmail.com>
2020-11-14 20:48:04 -0500

    Added a logout button to header component

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,11 +1,16 @@
 <template>
   <v-container>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <v-app-bar-nav-icon v-if="isAuthorized" @click="toggleSidebarDrawer(sidebar.drawerOpen)"></v-app-bar-nav-icon>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon
+        v-if="isAuthorized"
+        @click="toggleSidebarDrawer(sidebar.drawerOpen)"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>Dice Roller</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <template v-if="user.email">
+        {{ user.email }}
+        <v-icon class="ml-2" @click="logoutButton">mdi-logout</v-icon>
+      </template>
     </v-app-bar>
   </v-container>
 </template>
@@ -14,25 +19,25 @@
 import { mapActions, mapState } from 'vuex';
 
 export default {
-    name: 'Header',
-    components: {},
-    data: () => ({
-      drawer: false,
-    }),
-    computed: {
-      ...mapState(['appName', 'user', 'isAuthorized', 'sidebar']),
+  name: 'Header',
+  components: {},
+  data: () => ({
+    drawer: false,
+  }),
+  computed: {
+    ...mapState(['appName', 'user', 'isAuthorized', 'sidebar']),
+  },
+  async created() {},
+  methods: {
+    ...mapActions(['userLogout', 'toggleSidebarDrawer']),
+    ...mapActions('auth', ['logout']),
+    async logoutButton() {
+      await this.logout();
+      await this.userLogout();
+      this.$router.replace({ name: 'login' });
     },
-    async created() {},
-    methods: {
-      ...mapActions(['userLogout', 'toggleSidebarDrawer']),
-      ...mapActions('auth', ['logout']),
-      async logoutButon() {
-        await this.logout();
-        await this.userLogout();
-        this.$router.replace({ name: 'login' });
-      }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
@@ -47,10 +52,10 @@ export default {
 }
 
 .header-label {
-    align-self: center;
+  align-self: center;
 }
 
 .header-btn {
-    align-self: center;
+  align-self: center;
 }
 </style>
